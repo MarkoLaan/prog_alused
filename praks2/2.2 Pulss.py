d3d33c6a0d751fde587ff4cb7b859f6e42ac32ca
MarkoLaan <marko.laan@khk.ee>
2021-10-19 11:03:02 +0300

    2.2 Pulss

---
+++ praks2/2.2 Pulss.py
@@ -0,0 +1,23 @@
+vanus = int(input("sisestage vanus: "))
+sugu = input("sisestage sugu(M,m N,n): ")
+trentüüp = int(input("sisestage treeningu tüüp1 - tervisetreening, 2 - põhivastupidavuse treening, 3 - intensiivne aeroobne treening: "))
+
+if sugu == "M" or "m":
+    pulsemax = 220 - vanus
+    
+if sugu == "N" or "n":
+    pulsemax = 206 - vanus * 0.88
+    
+if trentüüp == 1:
+    minpulse = 0.5 * pulsemax
+    maxpulse = 0.7 * pulsemax
+elif trentüüp == 2:
+    minpulse = 0.7 * pulsemax
+    maxpulse = 0.8 * pulsemax
+elif trentüüp == 3:
+    minpulse = 0.8 * pulsemax
+    maxpulse = 0.87 * pulsemax
+    
+maxpulse = round(maxpulse)
+minpulse = round(minpulse)
+print("Pulsisagedus peab olema vahemikus " + str(minpulse) + " ja " + str(maxpulse) + " vahel.")
