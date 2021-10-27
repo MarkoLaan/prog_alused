943f948d52f319d8970b2417abb5b3829d990d54
MarkoLaan <marko.laan@khk.ee>
2021-10-27 11:57:50 +0300

    2.3 Istekoht

---
+++ praks2/2.3 Istekoht.py
@@ -0,0 +1,16 @@
+from random import randint
+
+valik = input("Kas soovite istekoha valida ise või loosi teel? (ise/loos) : ")
+if valik == "ise":
+    istekoht = input("Aknakoht või mitte (aken/muu) : ")
+elif valik == "loos":
+    koht = randint(1, 3)
+if koht == 1:
+    print ("Istekoht loositi. Aknakoht")
+elif koht == 2 or koht == 3:
+    print ("Istekoht loositi. Vahekäigukoht")
+    
+elif istekoht == "aken":
+    print ("Valisite ise. Aknakoht")
+elif istekoht == "muu":
+    print ("Valisite ise. Vahekäigukoht")
