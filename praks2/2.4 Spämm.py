3aa9f301cd154e979cdc7174e9fe45d954a826d6
MarkoLaan <marko.laan@khk.ee>
2021-10-27 11:58:27 +0300

    2.4 Spämm

---
+++ praks2/2.4 Spämm.py
@@ -0,0 +1,9 @@
+size = float(input("Kirja suurus: "))
+topic = input("Kirja teema pealkiri: ")
+file = input("Kas kiri on failiga(jah/ei): ")
+
+if size < 1.0:
+    if topic == "":
+        print("Kiri on spämm")
+    else:
+        print("Kiri ei ole spämm")
